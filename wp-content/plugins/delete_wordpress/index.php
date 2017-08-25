7714add6f6ca6ae934adc654520176955cdb1fca
Vicdotcom <iamvictorsandu@gmail.com>
2017-08-25 19:20:22 +0300

    Create Plugin

---
+++ wp-content/plugins/delete_wordpress/index.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+
+Plugin Name: Kill Wordpress Plugin
+Plugin URI: http://rvm.cu.cc
+Author: Vicdotcom
+Description: First Plugin Made
+
+*/
+
+add_shortcode('afisare_text','kwp_afisare_text');
+
+function kwp_afisare_text(){
+    
+    echo 'Ana are mere!';
+    
+}
