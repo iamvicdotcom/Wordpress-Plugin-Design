77c7f11debd3880111e19c13b1c5987f83b01812
iamvicdotcom <iamvictorsandu@gmail.com>
2017-09-22 16:11:56 +0300

    nu mai stiu ce am facut

--- wp-content/plugins/delete_wordpress/index.php
+++ wp-content/plugins/delete_wordpress/index.php
@@ -12,7 +12,8 @@ Description: First Plugin Made
 add_shortcode('afisare_text','kwp_afisare_text');
 add_filter('the_title','kwp_change_title');
 add_filter('the_content','kwp_facebook_like');
-
+add_action('admin_notices','kwp_notification');
+add_shortcode('imagini_random','kwp_random_banner');
 
 function kwp_afisare_text(){
     
@@ -25,7 +26,30 @@ function kwp_change_title(){
     
 }
 
-function kwp_facebook_like(){
+function kwp_facebook_like($content){
+    echo do_shortcode($content);
     echo '<iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2FOGTVY%2F&width=50&layout=button&action=like&size=large&show_faces=true&share=true&height=65&appId" width="200" height="65" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>';
 }
 
+function kwp_notification(){
+    
+    echo '<div class="notice notice-info">';
+    echo '<p>Please buy AK47</p>';
+        echo'</div>';
+    
+}
+
+
+function kwp_random_banner(){
+    
+    $images = array(
+    
+    'http://placehold.it/350x150/FF0000',
+    'http://placehold.it/350x150/00ff00',
+    'http://placehold.it/350x150/0000ff',
+    
+    );
+    
+    echo '<img src="'.$images[rand(0,2)].'">';
+    
+}
---
+++ wp-content/plugins/text_widget/index.php
@@ -0,0 +1,45 @@
+
+<?php
+/*
+    Plugin Name: Ipsum Generator   
+    Plugin URI: https://swissacademy.eu
+    Author: HaHaHa Programmer
+    Author URI: https://swissacademy.eu
+    Description: Generates a custom lore ispum text. Sooooo original. Shortcode example [ipsum paragrafe=5 lungime=short|medium|long|verylong]
+*/
+
+add_action('widgets_init', 'register_text_widget');
+
+function register_text_widget(){
+    register_widget('Text_ipsum');
+}
+
+
+class Text_ipsum extends WP_Widget {
+    
+    public function __construct(){
+        parent::__construct(
+        'text_ipsum', //id
+        'Text_ipsum', //Name
+        array('description' => 'This is a fun widget!')
+        
+        );
+        
+    }
+
+
+    public function widget($args, $instance){
+        // ce punem aici apare si pe site 
+            
+    }
+    
+    public function form($instance){
+        //ce apare aici afiseaza in backend la WP
+    }
+    
+    public function update($new_instance, $old_instance){
+        
+        
+    }
+}
+
