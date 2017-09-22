653e738be47036feb07ee1a495955c9589ed6200
iamvicdotcom <iamvictorsandu@gmail.com>
2017-09-22 13:41:12 +0300

    add comment

--- wp-content/plugins/delete_wordpress/index.php
+++ wp-content/plugins/delete_wordpress/index.php
@@ -15,17 +15,18 @@ add_filter('the_content','kwp_facebook_like');
 
 
 function kwp_afisare_text(){
-    
+
     echo 'Ana are mere!';
-    
+
 }
 
 function kwp_change_title(){
     echo '77777';
-    
+
 }
 
 function kwp_facebook_like(){
     echo '<iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2FOGTVY%2F&width=50&layout=button&action=like&size=large&show_faces=true&share=true&height=65&appId" width="200" height="65" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>';
 }
 
+// comment //
